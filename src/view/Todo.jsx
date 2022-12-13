958b5c9ebcb7c47b38291afd8eb5db21a9871021
Gelo <tmarkangelo96@gmail.com>
2022-12-14 02:01:58 +0800

    Render TOdoDetails Component

--- src/view/Todo.jsx
+++ src/view/Todo.jsx
@@ -1,20 +1,38 @@
 import { Wrapper } from "../components/Wrapper";
 import { Text } from "../components/Text";
 import { CheckBox } from "../components/CheckBox";
+import { useState } from "react";
+import { TodoDetails } from "./TodoDetails";
 
-export const Todo = ({ todoTitle, todoId }) => {
+export const Todo = ({ todoTitle, todoId, todoSummary }) => {
+  const [details, showDetails] = useState(false);
+  const handleClick = (event) => {
+    if (event.target.tagName === "INPUT") return;
+    showDetails(() => !details);
+  };
   return (
-    <Wrapper
-      paddingLeft="20px"
-      id={todoId}
-      height="100px"
-      width="80%"
-      shadow="3px"
-    >
-      <CheckBox />
-      <Text fontSize="50px" fontWeight="bold" color="#181D31">
-        {todoTitle}
-      </Text>
-    </Wrapper>
+    <>
+      <Wrapper
+        paddingLeft="20px"
+        id={todoId}
+        height="100px"
+        width="80%"
+        shadow="3px"
+        onClick={handleClick}
+        cursor="pointer"
+      >
+        <CheckBox />
+        <Text fontSize="50px" fontWeight="bold" color="#181D31">
+          {todoTitle}
+        </Text>
+      </Wrapper>
+      {details && (
+        <TodoDetails
+          todoId={todoId}
+          todoTitle={todoTitle}
+          todoDescription={todoSummary}
+        />
+      )}
+    </>
   );
 };
