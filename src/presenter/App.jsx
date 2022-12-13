9504ca286445eb111414011f1ed68baa3bda4529
Gelo <tmarkangelo96@gmail.com>
2022-12-13 19:25:14 +0800

    Add presenter

---
+++ src/presenter/App.jsx
@@ -0,0 +1,27 @@
+import { useReducer, useState } from "react";
+import { Wrapper } from "../components/Wrapper";
+import { Title } from "../view/Title";
+import { TodoList } from "../view/TodoList";
+import { AddTask } from "../view/AddTask";
+import { TodoForm } from "../view/TodoForm";
+import { useTodoForm } from "../model/useTodoForm";
+import { useTodos } from "../model/useTodos";
+
+const App = () => {
+  const { todos, updateTodos } = useTodos();
+  const [isFormVisible, { showForm, hideForm }] = useTodoForm({
+    visible: false,
+  });
+  return (
+    <Wrapper flexDirection="column">
+      <Title />
+      <TodoList todos={todos} />
+      {isFormVisible && (
+        <TodoForm handleClick={hideForm} handleSave={updateTodos} />
+      )}
+      <AddTask handleClick={showForm} />
+    </Wrapper>
+  );
+};
+
+export default App;
